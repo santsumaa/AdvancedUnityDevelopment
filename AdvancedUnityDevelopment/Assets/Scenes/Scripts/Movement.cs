63d44f2260a48cd3a0f2e4847bf01aa4b7de6c71
Santi <santiboy03@gmail.com>
2021-09-20 10:58:31 +0300

    Movemint

--- AdvancedUnityDevelopment/Assets/Scenes/Scripts/Movement.cs
+++ AdvancedUnityDevelopment/Assets/Scenes/Scripts/Movement.cs
@@ -4,37 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Vector2 speed = new Vector2(10, 10);
+    public float movementspeed = 10;
+    public float jumpforce = 200;
 
     private Vector2 movement = new Vector2(1, 1);
 
-    // Use this for initialization
+    Rigidbody2D rb;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
 
-        movement = new Vector2(
-            speed.x * inputX,
-            speed.y * inputY);
+
+        movement = new Vector2(movementspeed * inputX, rb.velocity.y);
 
         if (Input.GetKeyDown("space"))
         {
-            transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+            rb.AddForce(Vector2.up * jumpforce);
         }
 
     }
     void FixedUpdate()
     {
-        // 5 - Move the game object
-        GetComponent<Rigidbody2D>().velocity = movement;
-        //rigidbody2D.AddForce(movement);
+        
+        rb.velocity = movement;
+        
 
     }
 }
